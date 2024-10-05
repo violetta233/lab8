2757e0b71047b67418041146c2dcfe864acb869f
Лебедь Виолетта <wiolettalebed5@gmail.com>
2024-10-05 08:26:24 +0300

    summa time

---
+++ timer.pas
@@ -0,0 +1,13 @@
+﻿program task3;
+begin
+  var c:=readinteger;
+  var min:=readinteger;
+  var sec:=readinteger;
+  var sum:integer;
+  sum:=c*60*60 + min*60 + sec;
+  print(sum)
+end.
+{3
+20
+30
+12030 }
