030fadc39ed8500fe01266d62f77ca585882dfa1
Лебедь Виолетта <wiolettalebed5@gmail.com>
2024-10-05 08:21:28 +0300

    year2

--- time.pas
+++ time.pas
@@ -2,7 +2,13 @@
 begin
   var n:=readinteger;
   if ((n mod 4)=0) and ((n mod 100) <>0) and ((n mod 400)<>0) then
-    print('високосный')
+    print('366')
   else
-    print('не високосный')
+    print('365')
 end.
+
+{2023
+365 }
+
+{1937
+365 }
