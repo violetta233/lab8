a065d9580c41540790febe1b3db76db47014b1d9
Лебедь Виолетта <wiolettalebed5@gmail.com>
2024-10-05 08:04:01 +0300

    change hello logic

--- hello.pas
+++ hello.pas
@@ -1,1 +1,11 @@
-﻿
+﻿program task1;
+begin
+  var time:=readinteger;
+  assert((0<=time) and (time<=23));
+  case time of
+    4..10: Println('Доброе утро, мир!');
+    11..16: Println('Добрый день, мир!');
+    17..22: Println('Добрый вечер, мир!');
+    23, 0, 1, 2, 3: Println('Доброй ночи, мир!');
+   end;
+end.
